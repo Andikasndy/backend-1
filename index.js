74a0c591978ee8e75b8cb6c1cb97cf09b1ba746c
andika <kentungj97@gmail.com>
2024-02-05 00:51:48 +0700

    setup nodemon

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-const test = "Hello world"
-console.log(test)
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/subindo',(req, res) => {
+  res.send('Halo Dunia! ')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
