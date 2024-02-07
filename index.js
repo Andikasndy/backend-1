33a63a679e51d20a283155582b9f9ac4e5d1105e
andika <kentungj97@gmail.com>
2024-02-07 21:44:11 +0700

    setup postman

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser')
 
+
+app.use (bodyParser.json())
 app.get('/', (req, res) => {
+  console.log( req.query )
   res.send('Hello World!')
 })
 
 app.get('/subindo',(req, res) => {
   res.send('Halo Dunia! ')
+})
+
+app.post('/login',(req, res) =>{
+  console.log ( req.body )
+  res.send ('login berhasil')
+})
+app.put('/username',(req,res) =>{
+  console.log(req.body)
+  res.send('username berhasil di ubah')
 })
 
 app.listen(port, () => {
